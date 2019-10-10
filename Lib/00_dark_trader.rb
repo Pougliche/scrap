83911631bb7ea250cb70cd0a1b48647ab84a3e08
Pougliche <pugliese.viktor@gmail.com>
2019-10-10 19:13:03 +0200

    ajout de fichier avec des notes

--- Lib/00_dark_trader.rb
+++ Lib/00_dark_trader.rb
@@ -2,13 +2,12 @@ require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
-def bring_html(url)
+def bring_html(url)   #Bien penser à toujours remettre (url) à toutes les occurences 
 
         return page = Nokogiri::HTML(open(url))
 
 end
     
-
 def bitco(url)
 
 info=Hash.new 
@@ -17,7 +16,7 @@ all_crypto = bring_html(url).xpath("//html/body/div[2]/div[2]/div[1]/div[1]/div[
 
 all_crypto.each do |cryptodata|
 
-coin_name=cryptodata.css('td[class="no-wrap currency-name"]').css('a[class="currency-name-container link-secondary"]').text
+coin_name=cryptodata.css('td[class="no-wrap currency-name"]').css('a[class="currency-name-container link-secondary"]').text     #on utilise ici deux fois .css, faculcatif mais évite d'avoir tous les "a" de la page, juste les "a" de ce "tr". 
 
 coin_price=cryptodata.css("td[class='no-wrap text-right']").css('a[class="price"]').text
 
@@ -25,7 +24,7 @@ info[coin_name]=coin_price
 
     end
 
-    return info
+    return info         #On oublie pas de return le hash après la fin de la boucle mais avant la fin du def
 
 end
 
--- Lib/01_mairie_christmas.rb
+++ Lib/01_mairie_christmas.rb
@@ -16,9 +16,9 @@ end
 
     all_town.each do |townlisturl|
 
-    town_link = townlisturl["href"]
+    town_link = townlisturl["href"]         #on récupère tous les href des liens grâce à la boucle 
 
-    town_link = "http://annuaire-des-mairies.com#{town_link[1,town_link.length]}"
+    town_link = "http://annuaire-des-mairies.com#{town_link[1,town_link.length]}"       #pour lier les deux parties, on utilise #{} ; à l'intérieur on utilise 1.town_link.length pour commencer le lien au second caractère (le premier est un point)
 
         town_array << town_link
 
@@ -30,21 +30,21 @@ end
 
     def get_townhall_email(url)
 
-        finally=Hash.new
+        finally=Hash.new #toujours créer le hash en début de la def 
 
-    get_townhall_urls(url).each do |url_list|
+    get_townhall_urls(url).each do |url_list| #Fais une boucle dans la première méthode pour aller chercher dans toutes les URL 
 
-    page = Nokogiri::HTML(open(url_list))
+    page = Nokogiri::HTML(open(url_list)) #on utilise la méthode open sur l'url list pour incrémenter une variable page
 
-    all_mail = page.xpath('//html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-    town_name = page.xpath('//html/body/div/main/section[1]/div/div/div/h1').text
+    all_mail = page.xpath('//html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text 
+    town_name = page.xpath('//html/body/div/main/section[1]/div/div/div/h1').text           #pour récupérer les noms de ville & les mails, on utilise ensuite page avec le xpath correspondant
 
-        finally[town_name]=all_mail
+        finally[town_name]=all_mail #on ajoute les valeurs récupérées dans le hash
 
     end
 
-return finally
+return finally              #pfiou, finally !
 
 end
 
-puts get_townhall_email("http://annuaire-des-mairies.com/val-d-oise.html")
+puts get_townhall_email("http://annuaire-des-mairies.com/val-d-oise.html")      #on oublie pas de puts à la fin sinon rien ne s'affiche et on pleure des larmes de sang
